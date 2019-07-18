1a23c887796c3f1523874b4296217f3b5388ebd6
Nguyen Lam <nguyenchanhlam0811@gmail.com>
2019-07-18 11:29:52 +0700

    fixing after data fetched

--- src/pages/StorePage/ServiceDetailsPage.jsx
+++ src/pages/StorePage/ServiceDetailsPage.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import StoreDetailHeader from './StoreDetailHeader';
+import StoreDetailDescription from './StoreDetailDescription';
+
+import sample from '../../img/background-2.jpg';
 
 class ServiceDetailsPage extends Component {
     render() {
         return (
             <div>
-                <h1>this is service details page</h1>                
+                <StoreDetailHeader/>
+                <StoreDetailDescription/>
+
+                <p className="store__big-title">Sản Phẩm</p>
+                
+                <div className="store__product--sprout">
+                   <StoreDetailImage img= {sample} />
+                </div>
             </div>
         );
     }
---
+++ src/pages/StorePage/StoreDetailImage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StoreDetailImage extends Component {
+    render() {
+        return(
+            <div className="store__serviceImage--sprout">
+                <img src={this.props.img} alt="service-image" className="store__serviceImage--image"/>
+            </div>
+        );
+    }
+}
+
+export default StoreDetailImage;
